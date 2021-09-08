d7c8a97434fdbb2849d09b5622e3ee8584ef0617
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2021-09-08 12:16:29 -0400

    added animation to all 3 bamboo stalks and CSS'ed them into sitting pretty together

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -4,21 +4,16 @@ import { Link } from 'react-router-dom';
 import './home.css';
 
 import Mandarin from '../Card/card_1/card_1'
-import Bamboo from '../../Images/Bamboo.png'
+import Bamboo_left from '../../Images/Bamboo_left.png'
+import Bamboo_center from '../../Images/Bamboo_Center.png'
+import Bamboo_right from '../../Images/Bamboo_right.png'
 
 const Home = () => {
     return(
         <div className="Home">
-            <img src={Bamboo}/>
-            <div className="Content">
-                <div className="MyWork">
-                    <div className="Works">
-                        
-                    </div>
-                </div>
-
-
-            </div>
+            <img className="swing_b1" src={Bamboo_left}/>
+            <img className="swing_b2" src={Bamboo_center}/>
+            <img className="swing_b3" src={Bamboo_right}/>
         </div>
     )
 }
