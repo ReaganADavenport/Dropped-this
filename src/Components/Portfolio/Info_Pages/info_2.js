bba6910d9c25dceac909cfcbefeb6287f5fad089
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2021-10-26 14:56:05 -0400

    created info for DRC, have NOT hooked it up to app.js

---
+++ src/Components/Portfolio/Info_Pages/info_2.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+import { Link } from 'react-router-dom';
+import './info_2.css';
+
+
+const DRC_Info = () => {
+    return(
+        <div className='Body'>
+             <div className="Title">
+                <h1>Diakonia Retreat Center</h1>
+                <h4>Salem, South Carolina</h4>
+            </div>
+
+            <div className="Description">
+                <h2>Description:</h2>
+                <p>blah blah blah</p>
+            </div>
+
+        </div>
+        
+    )
+}
+
+export default DRC_Info;
