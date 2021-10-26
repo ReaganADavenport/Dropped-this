4f5e3c9d3e5c39c7d765f6487fdaf1697d12a5d9
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2021-10-26 15:07:19 -0400

    hooked up DRC info page with app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './Components/Header/header';
 import Home from './Components/Home/home';
 import Portfolio from './Components/Portfolio/portfolio';
+import DRC_Info from './Components/Portfolio/Info_Pages/info_2';
 import Footer from './Components/Footer/footer';
 
 import './App.css';
@@ -16,6 +17,7 @@ function App() {
         <Header></Header>
         <Route path='/' exact component={Home}/>
         <Route path='/portfolio' exact component={Portfolio}/>
+        <Route path='/info/DRC' exact component={DRC_Info}/>
         <Footer></Footer>
       </Router>
     </div>
